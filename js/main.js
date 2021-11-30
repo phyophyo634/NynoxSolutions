9ae97ae526ae79d2274a48feb41ae8059bd1f0b0
nyansawlinn1996 <51956676+nyansawlinn1996@users.noreply.github.com>
2021-11-30 13:18:37 +0630

    update photo

--- js/main.js
+++ js/main.js
@@ -80,11 +80,11 @@
 
   $(window).on('scroll', function () {
     var cur_pos = $(this).scrollTop();
-  
+
     nav_sections.each(function() {
       var top = $(this).offset().top - main_nav_height,
           bottom = top + $(this).outerHeight();
-  
+
       if (cur_pos >= top && cur_pos <= bottom) {
         main_nav.find('li').removeClass('active');
         main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('active');
@@ -106,7 +106,7 @@
     $('#portfolio-flters li').on( 'click', function() {
       $("#portfolio-flters li").removeClass('filter-active');
       $(this).addClass('filter-active');
-  
+
       portfolioIsotope.isotope({ filter: $(this).data('filter') });
     });
   });
@@ -130,3 +130,6 @@
 
 })(jQuery);
 
+function swap(image) {
+          document.getElementById("main").src = image.href;
+      }
